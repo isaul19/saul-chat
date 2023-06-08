189b883ce445b8dffdebc7255ef9025422130f21
isaul19 <saulhola348@gmail.com>
2023-06-08 11:04:19 -0500

    base v9

--- amplify/backend/function/gptTurboFunction/src/index.js
+++ amplify/backend/function/gptTurboFunction/src/index.js
@@ -9,9 +9,8 @@
 
 const { Configuration, OpenAIApi } = require("openai");
 
-const OPENAI_KEY = process.env.OPENAI_KEY;
 const configuration = new Configuration({
-  apiKey: OPENAI_KEY
+  apiKey: process.env.OPENAI_KEY
 });
 const openai = new OpenAIApi(configuration);
 
@@ -27,19 +26,30 @@ const buildResponse = ({ code, content }) => {
 };
 
 exports.handler = async (event /** @type {APIGatewayProxyEvent} */) => {
-  const prompt = event.body;
-  console.log("API_KEY:", OPENAI_KEY);
-  console.log("PROMTP:", prompt);
-  let openaiResponse = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: prompt }],
-    temperature: 0.9,
-    max_tokens: 2048,
-    frequency_penalty: 0.5,
-    presence_penalty: 0.5
-  });
-  console.log("RESPONSE:", openaiResponse);
+  try {
+    const prompt = event.body;
+    let openaiResponse = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      temperature: 0.9,
+      max_tokens: 2048,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.5
+    });
 
-  openaiResponse = openaiResponse.data.choices[0].message.content;
-  return buildResponse({ code: 200, content: openaiResponse });
+    openaiResponse = openaiResponse.data.choices[0].message.content;
+    return buildResponse({ code: 200, content: openaiResponse });
+  } catch (error) {
+    if (error.response && error.response.data && error.response.data.error) {
+      return buildResponse({
+        code: 500,
+        content: "Error en la clave de la API"
+      });
+    } else {
+      return buildResponse({
+        code: 500,
+        content: "Error en la llamada a la API"
+      });
+    }
+  }
 };
